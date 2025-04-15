29e5ad65c840136db28c04662499fe0db2422fcd
default <bojan@vukas.me>
2025-04-15 10:04:10 +0200

    merge not working

--- src/main/java/org/acme/ProblemStarter.java
+++ src/main/java/org/acme/ProblemStarter.java
@@ -5,15 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
-import io.smallrye.mutiny.GroupedMulti;
 import io.smallrye.mutiny.Multi;
-import io.smallrye.mutiny.Uni;
 
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.time.Duration;
-import java.util.List;
-import java.util.Scanner;
 import java.util.concurrent.Executors;
 
 import static java.lang.Boolean.TRUE;
@@ -23,7 +17,7 @@ public class ProblemStarter implements QuarkusApplication {
 
     private static final ObjectMapper mapper = new ObjectMapper();
 
-    public static List<JsonNode> createProblematicJson() {
+    public static JsonNode createProblematicJson() {
         try {
             JsonNode baseObject = mapper.readTree("{\"grouping_reason\":\"WHATEVER\"}");
 
@@ -34,15 +28,15 @@ public class ProblemStarter implements QuarkusApplication {
             for (int i = 0; i < 11; i++) {
                 arrayNode.add(baseObject.deepCopy()); // deepCopy to avoid reference sharing
             }
-            //!!!!!!!!!!!!!!!!!!!!!!!!!! >129 WORKS OK
-            for (int i = 0; i < 130; i++) {
+            //!!!!!!!!!!!!!!!!!!!!!!!!!! IF 120 replaced with 119 - IT WORKS
+            for (int i = 0; i < 120; i++) {
                 arrayNode.add(otherObject.deepCopy());
             }
 
-            return List.of(arrayNode);
+            return arrayNode;
         }
         catch (Exception e){
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
 
         return null;
@@ -50,31 +44,21 @@ public class ProblemStarter implements QuarkusApplication {
 
     public Multi<Multi<JsonNode>> transform() {
 
-        var str = createProblematicJson();
+        var problematicJson = createProblematicJson();
 
-        Multi<Uni<List<JsonNode>>> outer = Multi.createFrom().emitter(emitter -> {
-            for (JsonNode sublist : str) {
-                Uni<List<JsonNode>> subMulti = Multi.createFrom().iterable(sublist).collect().asList();
-                emitter.emit(subMulti);
-            }
-            emitter.complete();
-        });
-
-        return outer
+        return Multi.createFrom().item(Multi.createFrom().iterable(problematicJson).collect().asList())
                 .onItem().transform(a ->
                         a.onItem().transformToMulti(Multi.createFrom()::iterable)
                                 .log().group()
                                 .by(market -> market.has("grouping_reason"))
                                 .onItem()
-                                .invoke(item -> System.out.println(item))
-                                .onItem()
-                                //IF CONCAT REPLACED WITH MERGE - IT WORKS
-                                //.transformToMultiAndMerge(
-                                .transformToMultiAndConcatenate(
+                                //NOT EVEN MERGE WORKS - STRICTLY THE SIZE OF OTHER GROUP
+                                .transformToMultiAndMerge(
+                                //.transformToMultiAndConcatenate(
                                         group -> {
                                             return TRUE.equals(group.key())
-                                                    ? createSeparateMessageForEachVoidedMarket(group)
-                                                    : updateMessageToHaveOnlySettledMarkets(group);
+                                                    ? Multi.createFrom().item((JsonNode)mapper.createArrayNode())
+                                                    : Multi.createFrom().item((JsonNode)mapper.createArrayNode());
                                         })
                                 //NO MATTER THE DURATION - IT JUST HANGS
                                 .ifNoItem().after(Duration.ofSeconds(10)).fail().onFailure().invoke(throwable -> {
@@ -82,33 +66,8 @@ public class ProblemStarter implements QuarkusApplication {
                                     throwable.printStackTrace();
                                 }) // Add timeout
                                 .onCompletion()
-                                .invoke(() -> System.out.println("Settlements transformation completed"))
-
-                )
-
-                ;
-    }
-
-    private Multi<JsonNode> updateMessageToHaveOnlySettledMarkets(GroupedMulti<Boolean, JsonNode> settledMarketsGroup) {
-        return settledMarketsGroup
-                .onCompletion().invoke(() -> System.out.println("Completed 2"))
-                .collect()
-                .asList()
-                .onItem()
-                .transformToMulti(
-                        markets -> Multi.createFrom().item(new ObjectMapper().createObjectNode()));
-    }
-
-    private Multi<JsonNode> createSeparateMessageForEachVoidedMarket(GroupedMulti<Boolean, JsonNode> voidedMarketsGroup) {
-        return voidedMarketsGroup
-                .onCompletion().invoke(() -> System.out.println("Completed1"))
-                .onItem()
-                .transformToUniAndMerge(
-                        market -> Uni.createFrom().item(new ObjectMapper().createObjectNode()))
-                .collect()
-                .asList()
-                .onItem()
-                .transformToMulti(Multi.createFrom()::iterable);
+                                .invoke(() -> System.out.println("Transformation completed"))
+                );
     }
 
     @Override
